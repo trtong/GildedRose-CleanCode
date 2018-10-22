b5427f2acb52b4e25ba3014902244129fbabc8f2
Nhu Nguyen <nhu313@gmail.com>
2018-10-22 14:35:28 -0400

    convert to intellij

---
+++ src/test/java/com/zipcodewilmington/gildedrose/InventoryTest.java
@@ -0,0 +1,12 @@
+package com.zipcodewilmington.gildedrose;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InventoryTest {
+    @Test
+    public void updateQuantityTest(){
+        Assert.assertEquals(1, 1);
+    }
+}
+
