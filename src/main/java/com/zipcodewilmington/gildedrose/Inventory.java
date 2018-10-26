ec2209ddedbe40b9f1512a39e794260b4d91be5b
Trinh Tong <trinhtong@Zipcoders-MacBook-Pro-8.local>
2018-10-26 11:55:02 -0400

    Probably making it more complicated

--- src/main/java/com/zipcodewilmington/gildedrose/Inventory.java
+++ src/main/java/com/zipcodewilmington/gildedrose/Inventory.java
@@ -3,11 +3,102 @@ package com.zipcodewilmington.gildedrose;
 
 public class Inventory {
     private Item[] items;
+    static final String BRIE = "Aged Brie";
+    static final String BACK_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
 
     public Inventory(Item[] items) {
         super();
         this.items = items;
     }
+
+    // Updates the quality for 1 day
+    public void updateQuality() {
+
+        for (Item item: items) {
+            // make a switch statement
+
+            switch (item.getName()){
+
+            case BRIE:
+                if (!checkQuality(item, 1)) {
+                    adjustItemQuality(item, 1);
+                } else {
+                    setQualityMax(item);
+                }
+
+                decreaseSellIn(item);
+
+                break;
+
+            case BACK_PASS:
+                if (item.getSellIn() < 0) {
+                    setQualityZero(item);
+
+                } else if (item.getSellIn() <= 5) {
+                    if (!checkQuality(item, 2)) {
+                        adjustItemQuality(item, 2);
+                    } else {
+                        setQualityMax(item);
+                    }
+
+                } else if (item.getSellIn() <= 10) {
+                    if (!checkQuality(item, 3)) {
+                        adjustItemQuality(item, 3);
+                    } else {
+                        setQualityMax(item);
+                    }
+                }
+
+                decreaseSellIn(item);
+
+                break;
+
+
+            case SULFURAS:
+                item.setQuality(80);
+                item.setSellIn(item.getSellIn());
+
+
+
+            };
+        }
+    }
+
+    public void decreaseSellIn(Item name) {
+        name.setSellIn(name.getSellIn() - 1);
+
+    }
+
+    public boolean checkQuality(Item name, int addQuality) {
+        boolean isQuality50 = false;
+
+        if (name.getQuality() + addQuality < 50) {
+            isQuality50 = false;
+        } else {
+            isQuality50 = true;
+        }
+
+        return isQuality50;
+    }
+
+    public void setQualityMax(Item item) {
+        item.setQuality(50);
+    }
+
+    public void setQualityZero(Item item) {
+        item.setQuality(0);
+    }
+
+
+    public void adjustItemQuality(Item name, int qualityIncrement) {
+
+    }
+
+
+
+
 
 //    public void increaseQuality(int i){
 //
@@ -24,97 +115,113 @@ public class Inventory {
     }
 
 
-    // Old Code
-    public void updateQuality() {
-        // Giant for loop representing the "day" each day, the quality's of items change.
-
-        for (int i = 0; i < items.length; i++) {
-
-            // This if statement checks if it is NOT aged brie and NOT backstage passes
-            if (!items[i].getName().equals("Aged Brie")
-                    && !items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                // if it's not brie and bspasses
-                if (items[i].getQuality() > 0) {
-                    // checks if the quality is over 0
-                    if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
-                        // as long as it's not surlfuras...
-                        // quantity is decreased by 1
-                        items[i].setQuality(items[i].getQuality() - 1);
-                    }
-                }
 
 
 
-            } else {
-
-                // Next leg of IF statement quality < 50 then increase quality by 1
-
-                if (items[i].getQuality() < 50) {
-                    // if the quality is below 50
-                    items[i].setQuality(items[i].getQuality() + 1);
-                    // the quality is increased by 1
-
-                    // stuff about backstage passes
-                    if (items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
-                        // if the item is a backstagepass
-
-                        if (items[i].getSellIn() < 11) {
-                            // and if the backstage pass has to sell in 11 days
-                            if (items[i].getQuality() < 50) {
-                                // and the quality is below 50
-                                items[i].setQuality(items[i].getQuality() + 1);
-                                // then add 1 to the quality
-                            }
-                        }
-
-                        if (items[i].getSellIn() < 6) {
-                            // if the bspass has to sell in 6 days
-                            if (items[i].getQuality() < 50) {
-                                // and the quality is less than 50
-                                items[i].setQuality(items[i].getQuality() + 1);
-                                // then add 1 to the quality
-                            }
-                        }
-                    }
-                }
-            }
-
-            // another if the item IS NOT SULFURAS
-
-            if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].setSellIn(items[i].getSellIn() - 1);
-            }
-            // then decrease the sellin day by 1
 
 
-            // checks the items sell in
-            // if the items sell in is less than 0 (no sellin is negative)
-            if (items[i].getSellIn() < 0) {
 
-                if (!items[i].getName().equals("Aged Brie")) {
-                    // and the item IS NOT Brie
-                    if (!items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        // and the item IS NOT BSPASSES
-                        if (items[i].getQuality() > 0) {
-                            // if the item quality is greater than 0,
 
-                            if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
-                                // and the item is NOT SULFURAS,
-                                items[i].setQuality(items[i].getQuality() - 1);
-                                // the item quality -1
-                            }
-                        }
-                    } else { // if the item IS a backstage pass
-                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
-                    }
-                } else { // if the item IS BRIE
-                    if (items[i].getQuality() < 50) {
-                        // BRIE QUALITY is LESS THAN 50
-                        items[i].setQuality(items[i].getQuality() + 1);
-                        // Increase Brie's quality by 1
-                    }
-                }
-            }
-        }
-    }
+
+
+
+
+
+
+
+
+
+//    // Old Code
+//    public void updateQuality() {
+//        // Giant for loop representing the "day" each day, the quality's of items change.
+//
+//        for (int i = 0; i < items.length; i++) {
+//
+//            // This if statement checks if it is NOT aged brie and NOT backstage passes
+//            if (!items[i].getName().equals("Aged Brie")
+//                    && !items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                // if it's not brie and bspasses
+//                if (items[i].getQuality() > 0) {
+//                    // checks if the quality is over 0
+//                    if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+//                        // as long as it's not surlfuras...
+//                        // quantity is decreased by 1
+//                        items[i].setQuality(items[i].getQuality() - 1);
+//                    }
+//                }
+//
+//
+//
+//            } else {
+//
+//                // Next leg of IF statement quality < 50 then increase quality by 1
+//
+//                if (items[i].getQuality() < 50) {
+//                    // if the quality is below 50
+//                    items[i].setQuality(items[i].getQuality() + 1);
+//                    // the quality is increased by 1
+//
+//                    // stuff about backstage passes
+//                    if (items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
+//                        // if the item is a backstagepass
+//
+//                        if (items[i].getSellIn() < 11) {
+//                            // and if the backstage pass has to sell in 11 days
+//                            if (items[i].getQuality() < 50) {
+//                                // and the quality is below 50
+//                                items[i].setQuality(items[i].getQuality() + 1);
+//                                // then add 1 to the quality
+//                            }
+//                        }
+//
+//                        if (items[i].getSellIn() < 6) {
+//                            // if the bspass has to sell in 6 days
+//                            if (items[i].getQuality() < 50) {
+//                                // and the quality is less than 50
+//                                items[i].setQuality(items[i].getQuality() + 1);
+//                                // then add 1 to the quality
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            // another if the item IS NOT SULFURAS
+//
+//            if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+//                items[i].setSellIn(items[i].getSellIn() - 1);
+//            }
+//            // then decrease the sellin day by 1
+//
+//
+//            // checks the items sell in
+//            // if the items sell in is less than 0 (no sellin is negative)
+//            if (items[i].getSellIn() < 0) {
+//
+//                if (!items[i].getName().equals("Aged Brie")) {
+//                    // and the item IS NOT Brie
+//                    if (!items[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                        // and the item IS NOT BSPASSES
+//                        if (items[i].getQuality() > 0) {
+//                            // if the item quality is greater than 0,
+//
+//                            if (!items[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+//                                // and the item is NOT SULFURAS,
+//                                items[i].setQuality(items[i].getQuality() - 1);
+//                                // the item quality -1
+//                            }
+//                        }
+//                    } else { // if the item IS a backstage pass
+//                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
+//                    }
+//                } else { // if the item IS BRIE
+//                    if (items[i].getQuality() < 50) {
+//                        // BRIE QUALITY is LESS THAN 50
+//                        items[i].setQuality(items[i].getQuality() + 1);
+//                        // Increase Brie's quality by 1
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
--- src/test/java/com/zipcodewilmington/gildedrose/InventoryTest.java
+++ src/test/java/com/zipcodewilmington/gildedrose/InventoryTest.java
@@ -14,64 +14,98 @@ import java.util.Arrays;
 public class InventoryTest {
     Inventory inventory;
 
+    Item wine = new Item("wine", 2, 25);
+    Item agedBrie = new Item("Aged Brie", 1, 45);
+    Item passes = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 25);
+    Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 10, 80 );
+
+    Item[] items = {wine, agedBrie, passes, sulfuras};
+
     public InventoryTest() {
-
-        Item wine = new Item("wine", 2, 25);
-        Item agedBrie = new Item("Aged Brie", 1, 45);
-        Item passes = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 25);
-        Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 10, 80 );
-
-        Item[] items = {wine, agedBrie, passes, sulfuras};
-
         inventory = new Inventory(items);
     }
 
-    public void assertTestValues(Inventory inventory, int days, int wineSellIn, int wineQuality, int brieSellIn, int brieQuality,
-                                 int passSellIn, int passQuality, int sulfurasSellIn, int sulfurasQuality) {
-        for (int i = 0; i < days; i++) {
-            inventory.updateQuality();
-        }
-        Item[] items = inventory.getItems();
-        Item currentItem = items[0];
-        Assert.assertSame("wine", currentItem.getName());
-        Assert.assertEquals(wineSellIn, currentItem.getSellIn());
-        Assert.assertEquals(wineQuality, currentItem.getQuality());
+    @Test
+    public void testBrieQuality() {
+        // Brie quality increases as sell in decreases
+        // Given
 
-        currentItem = items[1];
-        Assert.assertSame("Aged Brie", currentItem.getName());
-        Assert.assertEquals(brieSellIn, currentItem.getSellIn());
-        Assert.assertEquals(brieQuality, currentItem.getQuality());
+        int expectedBrieQuality = 46;
+        String itemName = "agedBrie";
 
-        currentItem = items[2];
-        Assert.assertSame("Backstage passes to a TAFKAL80ETC concert", currentItem.getName());
-        Assert.assertEquals(passSellIn, currentItem.getSellIn());
-        Assert.assertEquals(passQuality, currentItem.getQuality());
+        // When
+        agedBrie.setQuality(agedBrie.getQuality() + 1);
+        int actualBrieQuality = agedBrie.getQuality();
 
-        currentItem = items[3];
-        Assert.assertSame("Sulfuras, Hand of Ragnaros", currentItem.getName());
-        Assert.assertEquals(sulfurasSellIn, currentItem.getSellIn());
-        Assert.assertEquals(sulfurasQuality, currentItem.getQuality());
+        // Then
+        Assert.assertEquals(expectedBrieQuality,actualBrieQuality);
     }
 
     @Test
-    public void updateQualityTest(){
-        // Given
+    public void testPassQuality() {
 
-        // One day
-        assertTestValues(inventory, 1, 1, 24, 0, 46, 10, 26, 10, 80);
-
-        // One more day
-        assertTestValues(inventory, 1, 0, 23, -1, 48, 9, 28, 10, 80);
-
-        // Four more days
-        assertTestValues(inventory, 4, -4, 15, -5, 50, 5, 36, 10, 80);
-
-        // Three more days
-        assertTestValues(inventory, 3, -7, 9, -8, 50, 2, 45, 10, 80);
-
-        // Five more days
-        assertTestValues(inventory, 5, -12, 0, -13, 50, -3, 0, 10, 80);
     }
 
 }
 
+// Test for previous code
+
+//
+//    public InventoryTest() {
+//
+//        Item wine = new Item("wine", 2, 25);
+//        Item agedBrie = new Item("Aged Brie", 1, 45);
+//        Item passes = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 25);
+//        Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 10, 80 );
+//
+//        Item[] items = {wine, agedBrie, passes, sulfuras};
+//
+//        inventory = new Inventory(items);
+//    }
+//
+//    public void assertTestValues(Inventory inventory, int days, int wineSellIn, int wineQuality, int brieSellIn, int brieQuality,
+//                                 int passSellIn, int passQuality, int sulfurasSellIn, int sulfurasQuality) {
+//        for (int i = 0; i < days; i++) {
+//            inventory.updateQuality();
+//        }
+//        Item[] items = inventory.getItems();
+//        Item currentItem = items[0];
+//        Assert.assertSame("wine", currentItem.getName());
+//        Assert.assertEquals(wineSellIn, currentItem.getSellIn());
+//        Assert.assertEquals(wineQuality, currentItem.getQuality());
+//
+//        currentItem = items[1];
+//        Assert.assertSame("Aged Brie", currentItem.getName());
+//        Assert.assertEquals(brieSellIn, currentItem.getSellIn());
+//        Assert.assertEquals(brieQuality, currentItem.getQuality());
+//
+//        currentItem = items[2];
+//        Assert.assertSame("Backstage passes to a TAFKAL80ETC concert", currentItem.getName());
+//        Assert.assertEquals(passSellIn, currentItem.getSellIn());
+//        Assert.assertEquals(passQuality, currentItem.getQuality());
+//
+//        currentItem = items[3];
+//        Assert.assertSame("Sulfuras, Hand of Ragnaros", currentItem.getName());
+//        Assert.assertEquals(sulfurasSellIn, currentItem.getSellIn());
+//        Assert.assertEquals(sulfurasQuality, currentItem.getQuality());
+//    }
+//
+//    @Test
+//    public void updateQualityTest(){
+//        // Given
+//
+//        // One day
+//        assertTestValues(inventory, 1, 1, 24, 0, 46, 10, 26, 10, 80);
+//
+//        // One more day
+//        assertTestValues(inventory, 1, 0, 23, -1, 48, 9, 28, 10, 80);
+//
+//        // Four more days
+//        assertTestValues(inventory, 4, -4, 15, -5, 50, 5, 36, 10, 80);
+//
+//        // Three more days
+//        assertTestValues(inventory, 3, -7, 9, -8, 50, 2, 45, 10, 80);
+//
+//        // Five more days
+//        assertTestValues(inventory, 5, -12, 0, -13, 50, -3, 0, 10, 80);
+//    }
